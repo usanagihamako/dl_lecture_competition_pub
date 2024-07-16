a382e9fc4cab3460fcdb4749587fc5aa0b76b12a
usanagihamako <hamaji-kosuke082@g.ecc.u-tokyo.ac.jp>
2024-07-16 07:35:06 +0000

    change epoch

--- main.py
+++ main.py
@@ -378,7 +378,7 @@ def main():
     model = VQAModel(vocab_size=len(train_dataset.question2idx)+1, n_answer=len(train_dataset.answer2idx)).to(device)
 
     # optimizer / criterion
-    num_epoch = 20
+    num_epoch = 1
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 
